ea3600118062616ea25260e0fb5e08352e2d860c
HuanyuKuo <hukuo@ucsd.edu>
2018-04-15 23:40:36 -0700

    finish_several_runs_for_s

--- dilution.py
+++ dilution.py
@@ -17,26 +17,31 @@ class Dilution:
         self.media = media
         self.neutral = neutral
         self.ref_bcid =  ref
-        self.ref_idx =  np.ones((2,self.N_GEN))   
+        self.ref_idx =  []# np.zeros((2,1))   
         self.ref_counts =  np.ones((2,self.N_GEN)) 
-        self.pvals = [] # initial barcode frequecy
-        self.Pvals = [] # sampling barcode freqency
+        self.pvals = [] # initial barcode frequecy (experimental value, and used as inital frequency in simualtion)
+        self.Pvals = [] # sampling barcode freqency (simulated value)
         self.all_count = []  # all reads, experiment value
         self.simu_reads = [] # simulated barcode reads
-        self.scaled_reads = [] # rescale simlated barcode reads to reference value
+        self.exp_reads = None
+        self.exp_ini_freq = None
+        self.sort_idx = [] # index of sorting of initial barcode freq (in decending order)
         self.sort_reads = []
+        self.sort_bcid0 = None
+        self.bcid0 = None
         self.s_record = None
+        self.exp_exist = None
         
     def runs(self):
-        self.read_files()
+        # import inital bc_freq from file
+        self.read_files()      
+        # simulate dilution and reads
         self.dilutions()
-        self.selection_coef()
-        # Optional Outputs and Display
-        #self.output_simu_reads()
-        #self.plot_ref_strain()  
-       
-    def get_freq(self, bc_count, gen):
-        return  bc_count/float(self.all_count[gen])
+        # calculate simulated selection coefficient
+        simu_scaled_reads = self.rescale_reads(self.simu_reads, self.ref_idx[0], self.ref_idx[1])
+        self.s_record = self.selection_coef(simu_scaled_reads)
+        
+        
     
     def input_dat(self, file):
         fopen = open(file,'r')  # open file
@@ -47,8 +52,11 @@ class Dilution:
         return dat
     
     def get_index(self,arr,val):
-        itemindex = np.where(arr == val)
-        return itemindex[0][0]
+        index = np.flatnonzero(arr == val)
+        if not index: # if is empty
+            return float('NaN')
+        else:
+            return index[0]
     
     def read_files(self):
         
@@ -70,18 +78,24 @@ class Dilution:
             bc_id = np.array(bc_id) # make format as numpy array
             bc_count = np.array(bc_count)  # make format as numpy array
             self.all_count.append(sum(bc_count)) # save all barcode counts
-            
-            # Find bc_count & idx of the reference:
-            for ref in range(0,1):
-                idx = self.get_index(bc_id, self.ref_bcid[ref])  
-                self.ref_idx[ref][gen]= idx
-                self.ref_counts[ref][gen] = bc_count[idx]
-            
-            # Record initial barcode freqnecy as "real" probability for samping
+           
+            # Record initial barcode freqnecy as "real" probability for samping, make it in decending order
             if gen ==0:
-                self.pvals = self.get_freq(bc_count,gen)
+                pvals = bc_count/float(self.all_count[gen]) # Record initial barcode freqnecy as "real" probability pvals
+                self.sort_index = np.argsort(bc_count) 
+                self.sort_index = self.sort_index[::-1] # reverse
+                self.pvals = pvals[self.sort_index] # make it in decending order
                 self.N_BCID = len(self.pvals)
-        
+                
+            # Find bc_count & idx of the reference:
+            for ref in range(0,2):
+                idx1 = self.get_index(bc_id, self.ref_bcid[ref])  # index of reference barcode in original file
+                self.ref_counts[ref][gen] = bc_count[idx1]
+                if gen ==0:
+                    idx2 = self.get_index(self.sort_index, idx1)      # index of sort_of_original_index 
+                    self.ref_idx.append(idx2)
+                    
+                 
     def sampling(self, freq, n):
         sample = np.random.multinomial(n, freq, size=1)
         return sample[0]
@@ -92,9 +106,9 @@ class Dilution:
     def dilutions(self):
         
         # before generation zero:    
-        f2 = np.vectorize(self.f)
+        get_int = np.vectorize(self.f) 
         self.Pvals.extend(self.pvals)
-        self.simu_reads.extend(f2(self.pvals*self.all_count[0]))
+        self.simu_reads.extend(get_int(self.pvals*self.all_count[0]))
         
         # start diluting and growing:
         tmp_pvals = self.pvals
@@ -104,39 +118,33 @@ class Dilution:
             if dl%2 == 1:  # 4 reads cause sampling fluctuation
                 self.simu_reads.extend(self.sampling(tmp_pvals,self.all_count[(dl+1)/2]))
                
-        self.Pvals = np.resize(self.Pvals,(self.N_DILUTE,self.N_BCID))
-        self.simu_reads = np.resize(self.simu_reads,(self.N_GEN,self.N_BCID))
+        self.Pvals = np.resize(self.Pvals,(self.N_DILUTE,self.N_BCID)) # deceding order as pvals
+        self.simu_reads = np.resize(self.simu_reads,(self.N_GEN,self.N_BCID)) # decending order as pvals
         
-    def rescale_reads(self):
-        ref1 = self.ref_idx[0][0]
-        ref2 = self.ref_idx[1][0]
+        # Force reference reads as experimental value:
+        for ref in range(0,2):
+            idx = self.ref_idx[ref]
+            for gen in range(0,self.N_GEN):
+                self.simu_reads[gen,idx] = self.ref_counts[ref][gen]
+                
+    def rescale_reads(self,reads,idx1,idx2):
+        scaled_reads =[]
         for gen in range(0,self.N_GEN):
-            reference_reads = self.simu_reads[gen,ref1]+self.simu_reads[gen,ref2]
-            self.scaled_reads.extend(self.simu_reads[gen,0:self.N_BCID] / float(reference_reads))
+            reference_reads = reads[gen,idx1]+reads[gen,idx2]
+            scaled_reads.extend(reads[gen,0:self.N_BCID] / float(reference_reads))
             
-        self.scaled_reads = np.resize(self.scaled_reads,(self.N_GEN,self.N_BCID))
+        scaled_reads = np.resize(scaled_reads,(self.N_GEN,self.N_BCID))
+        return scaled_reads
         
-    def sort_scaled_reads(self):
+    def selection_coef(self, scaled_reads):
         
-        self.rescale_reads()
-        
-        logcheck = np.sum(self.scaled_reads,axis=0)
-        logcheck_sort_indx = np.argsort(logcheck)
-        logcheck_sort_indx = logcheck_sort_indx[::-1]  # reverse
-        self.sort_reads = self.scaled_reads[0:self.N_GEN,logcheck_sort_indx]
-        itemindex = np.where(self.sort_reads[self.N_GEN-1,0:self.N_BCID]==0)
-        self.UPBOUND = itemindex[0][0]
-        self.sort_reads = self.sort_reads[0:self.N_GEN,0:self.UPBOUND]
-       
-        
-    def selection_coef(self):
-        self.sort_scaled_reads()
-        self.s_record = [0 for i in range(0,self.UPBOUND)]
+        s_record = [0 for i in range(0,self.N_BCID)]
         x = np.arange(0,45,10)
-        for bc in range(0,self.UPBOUND):
-            y = np.log(self.sort_reads[0:self.N_GEN,bc])
+        for bc in range(0,self.N_BCID):
+            y = np.log(scaled_reads[0:self.N_GEN,bc])
             z = np.polyfit(x, y, 1)
-            self.s_record[bc] = z[0]
+            s_record[bc] = z[0]
+        return s_record
         
     # -------------------#
     # Optional Functions #
@@ -145,6 +153,7 @@ class Dilution:
     def output_simu_reads(self): # Optional
         
         stitle = './source/' + self.replica + '_' + self.strain + '_' + self.media + '_gen0' + '.txt' # file name
+        
         dat = self.input_dat(stitle)
         
         stitle = './output/' + self.replica + '_' + self.strain + '_' + self.media + '_simulated_counts' + '.csv' # output file name
@@ -156,24 +165,39 @@ class Dilution:
         for idx_BCID in range(0,self.N_BCID):
             outstring =   self.strain + '\t' + dat[1+idx_BCID,0] + '\t' + dat[1+idx_BCID,2] + '\t' + self.replica + '_' + self.media
             for idx_gen in range(0,5):
-                outstring += '\t' + repr(self.simu_reads[idx_gen,idx_BCID])
+                bcid = int(dat[1+idx_BCID,0]) # get value of bacarod index 
+                idx_sorted = self.get_index(self.sort_bcid0, bcid) # search the sorted index for this bcid
+                outstring += '\t' + repr(self.simu_reads[idx_gen,idx_sorted])
             print outstring
         sys.stdout = orig_stdout
         fout.close()
+    
+    def sort_s_for_plot(self):
         
+        sort_indx = np.argsort(self.s_record)
+        #sort_indx = sort_indx[::-1]  
+        tmp = np.array(self.s_record)
+        tmp = tmp[sort_indx]
+        itemindex = np.flatnonzero(np.isnan(tmp))
+        UPBOUND = itemindex[0]
+        s_plot = tmp[0:UPBOUND]
+        return UPBOUND, s_plot
+       
     def plot_s_dist(self):  # Optional
-        width = 2*np.sqrt(np.var(self.s_record))
-        mean = np.mean(self.s_record)
-        sN = "N {0:0d}".format(len(self.s_record))
+        UPBOUND, s_plot = self.sort_s_for_plot()
+        
+        width = 2*np.sqrt(np.var(s_plot))
+        mean = np.mean(s_plot)
+        sN = "N {0:0d}".format(len(s_plot))
         sw = "Width {0:.4f}".format(width)
         sm = "Mean {0:0.5f}".format(mean)
         sr = "Reference strain slope {0:0.5f}".format(self.S_REF)
         plt.figure()
-        s_percent = [100*self.s_record[i] for i in range(0,self.UPBOUND)]
+        s_percent = [100*s_plot[i] for i in range(0,UPBOUND)]
         slabel = self.strain + '_' + self.replica + '_' + self.media
         plt.hist(s_percent, 16, label = slabel)
         plt.title('Histogram of selection coefficient')
-        plt.xlabel('selection coefficient (per generation) %')
+        plt.xlabel('s (per generation) %')
         plt.legend()
         plt.show()
         print (sN+'\n'+sw+'\n'+sm+'\n'+sr)
@@ -182,11 +206,13 @@ class Dilution:
         plt.figure()
         x = np.arange(0,45,10)
         col = plt.cm.jet(np.linspace(0,1,20)) 
-        for bc in range(0,self.UPBOUND):
-            y = np.log(self.sort_reads[0:self.N_GEN,bc])
+        scaled_reads = self.rescale_reads(self.simu_reads, self.ref_idx[0], self.ref_idx[1])
+        for bc in range(0,self.N_BCID):
             s = self.s_record[bc] 
-            cidx = int( (((0.1+s)>0)&((-0.1+s)<0))*(0.1+s)*100+ ((-0.1+s)>=0)*10)
-            plt.plot(x,y,color=col[cidx])
+            if np.isnan(s) == False: # if s is NOT Nan
+                y = np.log(scaled_reads[0:self.N_GEN,bc])
+                cidx = int( (((0.1+s)>0)&((-0.1+s)<0))*(0.1+s)*100+ ((-0.1+s)>=0)*10)
+                plt.plot(x,y,color=col[cidx])
         plt.title("Log of BC Freqeucy scaled by reference strains")
         plt.xlabel('generation')
         plt.show()
@@ -203,11 +229,11 @@ class Dilution:
         for ref in range(0,2):
             plt.figure()
             # Get (x,y) for dilution, (x2,y2) for reads, (x3, y3) for fit line
-            bc_freq = self.Pvals[0:self.N_DILUTE,self.ref_idx[ref][0]]
+            bc_freq = self.Pvals[0:self.N_DILUTE,self.ref_idx[ref]]
             y = np.log(bc_freq)        
             y_all += bc_freq
             for gen in range(0,self.N_GEN):
-                idx = self.ref_idx[ref][0]
+                idx = self.ref_idx[ref]
                 bc_freq = float(self.simu_reads[gen,idx])/float(self.all_count[gen])
                 y2[gen] = np.log(bc_freq)
                 y2_all[gen] += bc_freq
